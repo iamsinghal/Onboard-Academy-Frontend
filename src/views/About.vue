82ae338cf7a89fdb7d1965e4c622772273cb5a80
Rohit Singhal <rohit@focus21.io>
2018-06-07 23:02:04 -0400

    Welcome to Onboard Academy

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1 class="header">This is an about page</h1>
+    <v-icon>all_out</v-icon>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.header {
+  font-size: 24px;
+}
+</style>
